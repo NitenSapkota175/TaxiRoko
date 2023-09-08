3c01bfc270717f3363f1b975d3bb1c0998c5ff93
NitenSapkota175 <nitensapkota1705@gmail.com>
2023-09-09 01:38:02 +0530

    admin pannel link changed

--- TaxiRoko/urls.py
+++ TaxiRoko/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin_taxiroko_@2023', admin.site.urls),
     path('',include('cab_bookings.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
